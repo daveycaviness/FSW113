4f9bc5046b6ce28d5c071059ad4bf510f9bad0d1
daveycaviness <85713610+daveycaviness@users.noreply.github.com>
2021-10-22 02:14:19 -0400

    113 langs assignment

--- week4/assignments/langs/languages2.js
+++ week4/assignments/langs/languages2.js
@@ -1,19 +1,28 @@
 const lang = 'JavaScript'
 
-// Create an event listener for the submit button that adds all 'input' elements to 
+// Create an event listener for the submit button that adds all 'input' elements to
+ document.querySelector("#submit").addEventListener("click", function(){
+    myArry = [
+        ...document.querySelector('#lang0').value.split(" "),
+        ...document.querySelector('#lang1').value.split(" "),
+        ...document.querySelector('#lang2').value.split(" "),
+        ...document.querySelector('#lang3').value.split(" "),
+        ...document.querySelector('#lang4').value.split(" "),
+        ...document.querySelector('#lang5').value.split(" "),
+        ...document.querySelector('#lang6').value.split(" "),
+        ...document.querySelector('#lang7').value.split(" ")
+    ]
+    chkLang(myArry);
+});
 // a single array using the spread operator. Call the chkLang() function, passing in 
-// the array as an argument.
 
 
-function chkLang(langs) {
-    let result = false
-
-    // use an array method to check whether the user included 'JavaScript' in their
-    // list of languages
+function chkLang(langs) { 
+    let result = langs.some((element, index) => (langs[index].toLowerCase() === lang.toLowerCase()));
 
     let obj = document.querySelector('#TestResult')
-    if (result) 
+    if (result === true) 
         obj.innerText = `Congratulations!\nYou know ${lang}.`
     else
         obj.innerText = `Sorry,\nyou don't know ${lang}.`
-}
+};
