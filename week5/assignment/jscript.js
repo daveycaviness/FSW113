d90963b901c64ee9b463d9963ee52394cbf8d444
daveycaviness <85713610+daveycaviness@users.noreply.github.com>
2021-11-02 23:01:26 -0400

    resubmission fsw 113 classes and modules

--- week5/assignment/jscript.js
+++ week5/assignment/jscript.js
@@ -1,9 +1,8 @@
 // Declare any necessary variables.
-var score;
-var possScore;
+
 // Add am evemt listener that responds to the click of the "print" button by calling a function to instantiate
 //  a new student object, and another function to print the certificate.
-document.querySelector("#print").addEventListener("click", newStudent)
+document.querySelector("#print").addEventListener("click", () => certStudent(newStudent()))
     
 // Add an event listener that responds to the click of the reset button by resetting all the values
 // both in the form and in the certificate.
@@ -21,20 +20,21 @@ function reset(){
     }
         // Create a function that instantiates a new student object with the input from the HTML form.
 function newStudent(){
-    let myStudent = new Student()
-    certStudent(myStudent) 
+    let studentName = document.querySelector("#studentName").value
+    let studentCourse = document.querySelector("#className").value
+    let studentScore = myConvert(document.querySelector("#studentScores").value)
+    let possScore = myConvert(document.querySelector("#possibleScores").value)
+    return new Student(studentName, studentCourse, studentScore, possScore)  
 }
 // Create a function that fills in the student's name, class name, and calculated grade on the certificate .
 function certStudent(studentInfo){
-    document.querySelector("#certStudentName").innerHTML = studentInfo.name
-    document.querySelector("#certClassName").innerHTML = studentInfo.course
-    studentInfo.addScores()
-    studentInfo.addPossScores()
-    studentInfo.convertScores(score, possScore)  
+    document.querySelector("#certStudentName").textContent = studentInfo.getName() 
+    document.querySelector("#certClassName").textContent = studentInfo.getCourse()
+    document.querySelector("#certGrade").textContent = studentInfo.convertScores()
 }
 // Create a function that converts the contents of a comma-separated text string to a numeric array.
 // You can use this function when instantiating the arrays in the student object.
 
 function myConvert(arry){
-    arry.forEach((e, i) => (arry[i] = Number(arry[i])))
+    return arry.split(",").map(e => parseInt(e))
 }
--- week5/assignment/student.js
+++ week5/assignment/student.js
@@ -1,61 +1,31 @@
-// Create a class called Student whose constructor maintains all four data inputs from the HTML page.
-//The class should also contain the following methods
-// - a method that adds up all the student's scores
-// - a method that adds up all the possible scores
-// - a method that calculates the student's letter grade (divide the student's score by the possible scores and use the resulting decimal to determine grade)
-
 class Student{
-    constructor(){
-        this.name = document.querySelector("#studentName").value
-        this.course = document.querySelector("#className").value
-        this.scores = document.querySelector("#studentScores").value.split(",")
-        this.pScores = document.querySelector("#possibleScores").value.split(",")
+    constructor(name, course, scores, pScores){
+        this.name = name
+        this.course = course
+        this.scores = scores
+        this.pScores = pScores
     }
+    getName(){
+        return this.name
+    }
+
+    getCourse(){
+        return this.course
+    }
+
     addScores(){
-        myConvert(this.scores)
-        score = this.scores.reduce((preValue, currValue) => (preValue + currValue))
+       return this.scores.reduce((preValue, currValue) => (preValue + currValue))
     }
     addPossScores(){
-        myConvert(this.pScores)
-        possScore = this.pScores.reduce((preValue, currValue) => (preValue + currValue))
-        console.log(possScore)
+        return this.pScores.reduce((preValue, currValue) => (preValue + currValue)) 
     }
 
-    convertScores(recentScore, possiScore){
-        if ((recentScore / possiScore) * 100 >= 100){
-            document.querySelector("#certGrade").innerHTML = "A+"
-        }
-
-        else if ((recentScore / possiScore) * 100 >= 95 && (recentScore / possiScore) * 100 < 100){
-            document.querySelector("#certGrade").innerHTML = "A-"
-        }
-
-        else if ((recentScore / possiScore) * 100 >= 90 && (recentScore / possiScore) * 100 < 95){
-            document.querySelector("#certGrade").innerHTML = "B+"
-        }
-
-        else if ((recentScore / possiScore) * 100 >= 85 && (recentScore / possiScore) * 100 < 90){
-            document.querySelector("#certGrade").innerHTML = "B-"
-        }
-
-        else if ((recentScore / possiScore) * 100 >= 80 && (recentScore / possiScore) * 100 < 85){
-            document.querySelector("#certGrade").innerHTML = "C+"
-        }
-
-        else if ((recentScore / possiScore) * 100 >= 75 && (recentScore / possiScore) * 100 < 80){
-            document.querySelector("#certGrade").innerHTML = "C-"
-        }
-
-        else if ((recentScore / possiScore) * 100 >= 70 && (recentScore / possiScore) * 100 < 75){
-            document.querySelector("#certGrade").innerHTML = "D+"
-        }
-
-        else if ((recentScore / possiScore) * 100 >= 65 && (recentScore / possiScore) * 100 < 70){
-            document.querySelector("#certGrade").innerHTML = "D-"
-        }
-
-        else {
-            document.querySelector("#certGrade").innerHTML = "F"
-        } 
+    convertScores(){
+        var myScore = (this.addScores()/this.addPossScores())*100
+        if (myScore > 90) return "A"
+        if (myScore > 80) return "B"
+        if (myScore > 70) return "C"
+        if (myScore > 60) return "D"
+        else return "F"
     }
 }
